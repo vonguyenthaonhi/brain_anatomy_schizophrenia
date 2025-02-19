2589938a38337da066acb4510c53e24972b1f68a
Vo Nguyen Thao Nhi <139818124+vonguyenthaonhi@users.noreply.github.com>
2025-02-19 14:46:58 +0100

    Update estimator.py

--- submissions/starting_kit/estimator.py
+++ submissions/starting_kit/estimator.py
@@ -1,36 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 30 02:11:17 2022
-
-@author: edouard.duchesnay@cea.fr
-"""
-
+ 
 # import numpy as np
-
+ 
 from sklearn.base import BaseEstimator
 from sklearn.base import TransformerMixin
-from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import VotingClassifier
+from sklearn.linear_model import LogisticRegression
+ 
+from sklearn.svm import SVC, NuSVC
 from sklearn.pipeline import make_pipeline
-
-
+ 
+    
 class ROIsFeatureExtractor(BaseEstimator, TransformerMixin):
     """Select only the 284 ROIs features:"""
-
+ 
     def fit(self, X, y):
         return self
-
+ 
     def transform(self, X):
         return X[:, :284]
-
-
+ 
+ 
 def get_estimator():
     """Build your estimator here."""
     estimator = make_pipeline(
-        ROIsFeatureExtractor(),
-        MLPClassifier(random_state=1,
-                      hidden_layer_sizes=(200, 150, 100, 50, 25, ),
-                      )
-    )
-
+      ROIsFeatureExtractor(),
+      StandardScaler(),
+      VotingClassifier(estimators=[
+          ('logreg', LogisticRegression(max_iter=1000, penalty='elasticnet', solver='saga', l1_ratio=0.8, C=0.1)),  
+          ('nu_svc', NuSVC(kernel = 'rbf', class_weight="balanced", nu=0.6, gamma=0.00005, probability=True)), 
+          ('svm', SVC(class_weight = 'balanced', kernel = 'poly', gamma='scale', C=8, probability=True))
+      ], voting='soft')
+  )
     return estimator
